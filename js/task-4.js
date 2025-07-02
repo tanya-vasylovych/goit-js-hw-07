9bdac5ea8bc22a972622e1603c0c15dedcb376bb
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-07-02 14:06:16 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+  
+  if (login === "" || password === "") {
+    return alert("All form fields must be filled in");
+  }
+
+  console.log(`Login: ${login}, Password: ${password}`);
+  form.reset();
+}
